3d8b2f47322cf163e0c350e02a36940c2b336b01
Vukasin Crnogorac <crnogoracvukasin@hotmail.com>
2020-04-01 13:23:55 +0200

    pripremio za testiranje

---
+++ testiranje/instance2/string10_sgd/string10_sgd.sol
@@ -0,0 +1,11 @@
+
+
+
+Komandna linija: test vns string10_sgd 1 5 20000 5000 200 0.5 11111 0 
+Instanca: string10_sgd
+Izvrsavanje je pocelo: Wed Apr  1 12:48:24 2020
+ Zavrseno Wed Apr  1 12:51:15 2020
+ Trajalo je:         171.5880 sekundi   Broj iteracija=5000  LS iteracija=0
+Konacno resenje je dobijeno u Wed Apr  1 12:48:24 2020
+ posle           0.6120 sekundi  Broj iteracija=0  LS iteracija=0
+Funkcija cilja je:   86964     Suma fiksiranih grana:   86958    Razlika: 6
